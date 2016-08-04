d65e902ad76dce47f475cf0cb7c0361a622c9c47
Galvin Gao <galvingaoedu@icloud.com>
2016-08-04 22:45:10 +0800

    {v0.2.2}Big release
                + Added History cleaner
                * Improve menu UI
                + Added FPS Lock [Still in construction...]
                + Added test running state
                * Improve history search
                * Improve test x times, added value checker
                + Added custom test
                - Removed Test Counter (old)
                + Added Section Test Counter
                + Added Global Test Counter
                * Improve running button
                + Added Global Time Used Counter

--- main.lua
+++ main.lua
@@ -2,9 +2,7 @@
 
     iPad Performance Tester
 
-        Version: v0.2.0
         Author: Galvin Gao
-        Update Time: 2016.6.1 10:24
         GitHub: http://github.com/GalvinGao/iPT
 
 
@@ -16,51 +14,93 @@
 
 ]]--
 
-supportedOrientations( LANDSCAPE_RIGHT )  
+supportedOrientations( LANDSCAPE_ANY )  
 
 function setup()
+    -- Save Project Info
     saveProjectInfo( "Description", "This program is designed for testing iPad performance. The principle is the iPad will find the power of pi for 50,000,000 times and measure the time it takes." )
     saveProjectInfo( "Author", "Galvin Gao" )
-    testHaveBeenTested = 0
-    Enter_Specilified_Time_for_Searching = 0
-    fP = 0
-    LT = 0
+    
+    -- Setup Variable
+    setupVar()
+    
+    -- Menu Setup - Parameter
     parm()
 end
 
+function setupVar()
+    -- Setup Section Test Counter
+    SectionTestCounter = 0
+    
+    -- Setup Searching Enter Box
+    Enter_Specilified_Time_for_Searching = 0
+    
+    -- Setup FPS Calculate materials - FramesPassed
+    fP = 0
+    
+    -- Setup FPS Calculate materials - LastTime(ElapsedTimeNeedToMinus)
+    LT = 0
+    
+    -- Setup Global Test Counter
+    GlobalTestCounter = 0
+    
+    -- Setup Test Counter Var1
+    SaveTimeGC = 0
+    
+    -- Setup Test Counter Var2
+    LastTimeGC = 0
+    
+    -- Setup Test Counter Var3
+    SaveTimeGT = 0
+    
+    -- Setup Test Counter Var4
+    LastTimeGT = 0
+end
+
 function draw()
+    -- FPS Calculation
     fP = fP + 1
     t = ElapsedTime - LT
     fps = fP / t
-    if fP > 120 then
+    if fP > 60 then
         fP = 0
         t = t - LT
         fps = fP / t
         LT = ElapsedTime
         else
     end
-    background(100, 120, 160, 150)
-    font("Futura-MediumItalic")
-    fill(255)
-    fontSize(20)
-    textWrapWidth(500)
-    text("This test will make your iPad to find the power of π for 50000000 times.\n\nPlease mind, the test may take a while to run, please be patient.\n\n\nAverage:\n8.657363s", WIDTH/2, HEIGHT/2)
-    fontSize(11)
-    font("CourierNewPS-BoldMT")
-    text("FPS: "..fps, WIDTH/2+WIDTH/4, HEIGHT/2+WIDTH/4)
+    
+    -- Setup Background & font properties
+    background( 100, 120, 160, 150 )
+    font( "Futura-MediumItalic" )
+    fill( 255 )
+    fontSize( 20 )
+    textWrapWidth( 500 )
+    
+    -- Shows the discription
+    text( "This test will make your iPad to find the power of π for 50000000 times.\n\nPlease mind, the test may take a while to run, please be patient.\n\n\nAverage:\n8.657363s", WIDTH/2, HEIGHT/2 )
+    
+    -- Shows the FPS
+    fontSize( 11 )
+    font( "CourierNewPS-BoldMT" )
+    text( "FPS: "..fps, WIDTH/2+WIDTH/4, HEIGHT/2+WIDTH/4 )
 end 
 
 ----- [Start] Menu area -----
 
+-- Menu
+
 function parm()
     parameter.clear()
     parameter.watch( "fps" )
-    parameter.watch( "testHaveBeenTested" )
-    parameter.action( "Test once...", test )
+    parameter.action( "Quick Test...", test )
     parameter.action( "...  More Test Options  ...", more_test_menu_parm )
+    parameter.action( "+++ Test Counter +++", test_counter_parm )
     parameter.action( "---  History  ---", history_menu_parm )
     parameter.action( "|||  Clear Output  |||", clearOutput )
 end
+
+-- More Test Menu
 
 function more_test_menu_parm()
     parameter.clear()
@@ -68,7 +108,10 @@ function more_test_menu_parm()
     parameter.action( "Start 10x Test", test10times )
     parameter.action( "Start 25x Test", test25times )
     parameter.action( "Start 100x Test", test100times )
+    parameter.action( "Custom test...", customtest_prepare )
 end
+
+-- History Menu
 
 function history_menu_parm()
     parameter.clear()
@@ -76,7 +119,54 @@ function history_menu_parm()
     parameter.action( "Read Last Time Result", readLastestResult )
     parameter.integer( "Specilified_Time_to_Search", 1, 100, "" )
     parameter.action( "Read Specilified Time Result", readSpecilifiedResult )
+    parameter.action( "Clear all result", clearAllResult )
 end
+
+-- Test Counter Menu
+
+function test_counter_parm()
+    ReadGlobalCounter()
+    ReadGlobalTime()
+    parameter.clear()
+    parameter.action( "== Back ==", parm )
+    parameter.watch( "SectionTestCounter" )
+    parameter.watch( "GlobalTestCounter" )
+    parameter.watch( "GlobalTimeUsed" )
+end
+
+-- Test Counter Menu - Global Counter Reader
+
+function ReadGlobalCounter()
+    GlobalTestCounter = readLocalData( "GlobalCounter" )
+end
+
+-- Test Counter Menu - Global Counter Saver
+
+function SaveGlobalCounter()
+    LastTimeGC = readLocalData( "GlobalCounter" )
+    SaveTimeGC = LastTimeGC + 1
+    saveLocalData( "GlobalCounter", SaveTimeGC )
+    LastTimeGC = 0
+    SaveTimeGC = 0
+end
+
+-- Test Counter Menu - Global Time Used Reader
+
+function ReadGlobalTime()
+    GlobalTimeUsed = readLocalData( "GlobalTime" )
+end
+
+-- Test Counter Menu - Global Time Used Saver
+
+function SaveGlobalTime()
+    LastTimeGT = readLocalData( "GlobalTime" )
+    SaveTimeGT = LastTimeGT + passedtime
+    saveLocalData( "GlobalTime", SaveTimeGT )
+    LastTimeGT = 0
+    SaveTimeGT = 0
+end
+
+-- Clear Output
 
 function clearOutput()
     output.clear()
@@ -88,13 +178,18 @@ end
 
 ----- [Start] Test front -----
 
+-- Test Running Replace Button
+
 function testRunningButton()
     parameter.clear()
-    parameter.watch( "testHaveBeenTested_TestIsRunning..." )
-    parameter.action( "Test is running" )
-    parameter.action( "Test is running." )
-    parameter.action( "Test is running.." )
     parameter.action( "Test is running..." )
+    parameter.action( "--------------------------------------------" )
+    parameter.action( "Please be patient, the test " )
+    parameter.action( "may take a while to run." )
+end
+
+function CancelTest()
+    close()
 end
 
 ----- [End] Test front -----
@@ -103,7 +198,7 @@ end
 
 ----- [Start] Test area -----
 
--- Test 001x
+-- Test 1 Time
 
 function test()
     testRunningButton()
@@ -115,13 +210,13 @@ function test()
     nowtime = os.clock()
     passedtime = nowtime - passtime
     print( passedtime .. " s" )
-    testHaveBeenTested = testHaveBeenTested + 1
+    SectionTestCounter = SectionTestCounter + 1
     saveWithIndex()
     saveLastTime()
     parm()
 end
 
--- Test 010x
+-- Test 10 Times
 
 function test10times()
     testRunningButton()
@@ -134,14 +229,14 @@ function test10times()
         nowtime = os.clock()
         passedtime = nowtime - passtime
         print( i.."th test is done.\nResult: "..passedtime.."s\nFPS: "..fps )
-        testHaveBeenTested = testHaveBeenTested + 1
+        SectionTestCounter = SectionTestCounter + 1
         saveWithIndex()
         saveLastTime()
     end
     parm()
 end
 
--- Test 025x
+-- Test 25 Times
 
 function test25times()
     testRunningButton()
@@ -157,14 +252,14 @@ function test25times()
         nowtime = os.clock()
         passedtime = nowtime - passtime
         print( i.."th test is done.\nResult: "..passedtime.."s\nFPS: "..fps )
-        testHaveBeenTested = testHaveBeenTested + 1
+        SectionTestCounter = SectionTestCounter + 1
         saveWithIndex()
         saveLastTime()
     end
     parm()
 end
 
--- Test 100x
+-- Test 100 Times
 
 function test100times()
     testRunningButton()
@@ -180,7 +275,57 @@ function test100times()
         nowtime = os.clock()
         passedtime = nowtime - passtime
         print( i.."th test is done.\nResult: "..passedtime.."s\nFPS: "..fps )
-        testHaveBeenTested = testHaveBeenTested + 1
+        SectionTestCounter = SectionTestCounter + 1
+        saveWithIndex()
+        saveLastTime()
+    end
+    parm()
+end
+
+-- Custom test - Preparation / Setup & Ask for input
+
+function customtest_prepare()
+    parameter.clear()
+    testRunningTimes = 0
+    parameter.action( "== Back ==", more_test_menu_parm )
+    parameter.integer( "testRunningTimes", 1, 100, 1 )
+    parameter.integer( "testHowManyTimesDividePi_timed1000000", 1, 100, 50, customtest_dividetimes )
+    parameter.watch( "testHowManyTimesDividePi" )
+    parameter.action( "Submit", customtest_check )
+end
+
+-- Custom test - Divide times calculation & Variable set
+
+function customtest_dividetimes()
+    testHowManyTimesDividePi = testHowManyTimesDividePi_timed1000000 * 1000000
+end
+
+-- Custom test - Value Check
+
+function customtest_check()
+    if testRunningTimes == 0 or testRunningTimes == 1 then
+        parameter.clear()
+        parameter.action( "== Back ==", customtest_prepare )
+        parameter.action( "Illegal input!" )
+    else
+        customtest()
+    end
+end
+
+-- Custom test
+
+function customtest()
+    testRunningButton()
+    for i = 1, testRunningTimes do
+        passtime = os.clock()
+            for i = 1, testHowManyTimesDividePi do
+                pi = math.pi
+                math.sqrt( pi )
+            end
+        nowtime = os.clock()
+        passedtime = nowtime - passtime
+        print( i.."th test is done.\nResult: "..passedtime.."s\nFPS: "..fps )
+        SectionTestCounter = SectionTestCounter + 1
         saveWithIndex()
         saveLastTime()
     end
@@ -191,31 +336,65 @@ end
 
 
     
------ [Start] Data save -----
+----- [Start] Data edit -----
+
+-- Save with Index
 
 function saveWithIndex()
-    saveLocalData( "result_"..testHaveBeenTested, passedtime )
+    SaveGlobalCounter()
+    SaveGlobalTime()
+    saveLocalData( "result_"..SectionTestCounter, passedtime )
+    saveLocalData( "devide_"..SectionTestCounter, devidetimes )
 end
+
+-- Save Last Time
 
 function saveLastTime()
     saveLocalData( "lastTimeResult", passedtime )
 end
 
------ [End] Data save -----
+-- Clear all result
+function clearAllResult()
+    parameter.clear()
+    parameter.action( "Are you sure to clean all results?" )
+    parameter.action( "Yes", clearAllResultConfirmed )
+    parameter.action( "No", history_menu_parm )
+end
+
+function clearAllResultConfirmed()
+    clearLocalData()
+    parameter.clear()
+    parameter.action( "History Deleted." )
+    parameter.action( "= Back to Main Menu =", parm )
+end
+
+----- [End] Data edit -----
 
 
 
 ----- [Start] Data read -----
 
+-- Read Last Time Result
+
 function readLastestResult()
     LTR = readLocalData( "lastTimeResult" )
-    print( "Last time result: "..LTR.."s")
+    if LTR == nil then
+        print( "Data Not Found! Please run a test first!" )
+    else
+        print( "Last time result: "..LTR.."s")
+    end
 end
+
+-- Read Specilified Result
 
 function readSpecilifiedResult()
     dataKey = "result_"..Specilified_Time_to_Search
-    data = readLocalData( dataKey, "Data Not Found!" )
-    print( Specilified_Time_to_Search.."th result is: "..data.."s" )
+    result = readLocalData( dataKey )
+    if result == nil then
+        print( "Can't find " .. Specilified_Time_to_Search .. "th info. Please check your input." )
+    else
+        print( Specilified_Time_to_Search.."th test info:\nResult: "..result.."s" )
+    end
 end
 
 ----- [End] Data read -----
